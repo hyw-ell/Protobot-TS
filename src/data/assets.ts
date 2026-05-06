9d91e63574e43930297b45526061bcc146d25cfe
hyw-ell <31523573+hyw-ell@users.noreply.github.com>
2026-05-06 09:06:20 -0700

    Remove incorrect filter messing up image name and image mapping

--- src/data/assets.ts
+++ src/data/assets.ts
@@ -7,9 +7,7 @@ const imagePromises = imageFilePaths.map(f => loadImage(f))
 const resolvedImages = await Promise.all(imagePromises)
 
 export const images = Object.fromEntries(
-    imageFilePaths
-        .filter(f => /Misc_Icons|Defense_Icons|Difficulty_Icons/.test(f))
-        .map((filePath, index) => [path.basename(filePath), resolvedImages[index]])
+    imageFilePaths.map((filePath, index) => [path.basename(filePath), resolvedImages[index]])
 )
 
 const BASE_URL = 'https://raw.githubusercontent.com/hyw-ell/Protobot-TS/refs/heads/main/'
